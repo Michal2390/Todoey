8f68aef1a61c14f3d9a651ca036a21e7816fa181
Michał Fereniec <michalkoks99@gmail.com>
2022-05-13 13:36:21 +0200

    Added itemArray with nice love letters to my ni**as <3

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -10,8 +10,30 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
+    let itemArray = ["Kocham Frania","Kocham Bartusia","Kocham Patryka","Kocham Antka","Kocham Adiego","Kocham Arka","Kocham Krzysia","Kocham profesora Dąbrowskiego"]
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.navigationBar.barTintColor = .systemPurple
+        
+        
     }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
 }
 
