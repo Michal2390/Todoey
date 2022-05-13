5bf0bfd912f0a780007f9027d4af323a68d179d0
Michał Fereniec <michalkoks99@gmail.com>
2022-05-13 13:58:19 +0200

    Added checkmark with animation after pressing specific element

--- Todoey/TodoListViewController.swift
+++ Todoey/TodoListViewController.swift
@@ -28,5 +28,21 @@ class TodoListViewController: UITableViewController {
         return cell
     }
     
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+        
+    }
+    
 }
 
