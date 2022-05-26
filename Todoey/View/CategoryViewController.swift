e3ae60337ff0afdab02c07d2948199b6c91960b4
Michał Fereniec <michalkoks99@gmail.com>
2022-05-26 02:50:16 +0200

    setting up CategoryViewController to prepare it to further development

--- Todoey/View/CategoryViewController.swift
+++ Todoey/View/CategoryViewController.swift
@@ -7,19 +7,76 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoryViewController: UITableViewController {
 
+    
+    var itemArray = [Item]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
-
     
+    
+    
+    
+    
+    //MARK: - TableView DataSource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+        return cell
+    }
+    //MARK: - Data Manipulation Methods
+
+    func saveItems(){
+        
+        
+        do {
+            try context.save()
+        } catch {
+           print("Error saving context \(context)")
+        }
+
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()){
+
+        do{
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+
+        tableView.reloadData()
+
+    }
+    
+    //MARK: - Add New Categories
     
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         
     }
+    
+    //MARK: - TableView Delegate Methods
+    
+
     
 }
